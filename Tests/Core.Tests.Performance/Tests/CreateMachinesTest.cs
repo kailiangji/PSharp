e4607a847a37ddb04ed3941d81c37a729003001a
Pantazis Deligiannis <pdeligia@microsoft.com>
2018-09-06 14:56:45 +0100

    fixed license with stylecop

--- Tests/Core.Tests.Performance/Tests/CreateMachinesTest.cs
+++ Tests/Core.Tests.Performance/Tests/CreateMachinesTest.cs
@@ -1,6 +1,6 @@
 ﻿// ------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// Licensed under the MIT license. See License.txt in the repo root for full license information.
 // ------------------------------------------------------------------------------------------------
 
 using System.Threading;
